cad1c5c6aa298585c348a8078896a3cc0aceff8e
Palamax <stephanevalery81@gmail.com>
2017-07-21 10:34:25 +0200

    exo pipes

--- src/app/plan/program/exercices-list/exercices-list.component.ts
+++ src/app/plan/program/exercices-list/exercices-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Exercice} from "../../../shared/program/exercice";
 
 @Component({
@@ -8,6 +8,7 @@ import {Exercice} from "../../../shared/program/exercice";
 })
 export class ExercicesListComponent implements OnInit {
 
+  @Input()
   exercices: Exercice[] = [];
 
   constructor() { }
